64b0a6d52ebcc6b16e61eb3323ff001f21c2be34
VQuine <01010110.v@gmail.com>
2020-05-28 20:16:01 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,26 +11,61 @@ def select_name_and_motto_of_char_with_longest_motto
   "
   SELECT name, motto
   FROM characters
-  ORDER BY LEN(motto)
+  ORDER BY motto
   LIMIT 1
-
-
   ;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "
+  SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
+  LIMIT 1  
+  ;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "
+  SELECT authors.name, subgenres.name
+  FROM authors, subgenres
+  INNER JOIN series ON series.subgenre_id = subgenres.id AND series.author_id = authors.id
+  ;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+"
+SELECT series.title
+ FROM series
+
+INNER JOIN books 
+ ON books.series_id = series.id
+
+INNER JOIN character_books
+ ON character_books.book_id = books.id
+ 
+INNER JOIN characters
+ ON characters.id = character_books.character_id
+
+  WHERE species = 'human'
+  GROUP BY series.title
+
+  LIMIT 1  
+;"
+#you're inside a string here.. so be careful- NO DOUBLE QUOTING use single quotes for values
+#ALSO, I can't see how to ORDER BY COUNT(characters.species = 'human') or whatever..
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  
+  "SELECT characters.name, COUNT(character_books.character_id)
+  FROM characters
+  INNER JOIN character_books
+  ON character_books.character_id = characters.id
+  GROUP BY characters.name
+  ORDER BY COUNT(character_books.character_id) DESC
+  ;
+  "
+  # so proud of myself... ::sob::
 end
